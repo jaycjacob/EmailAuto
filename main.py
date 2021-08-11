24934259754cefcb1f9ee6548b3169a53a0c70d0
Jacob Kipchirchir <jaycjacob@gmail.com>
2021-08-11 13:31:51 +0300

    text/html alternative

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 import smtplib, ssl
 from getpass import getpass
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-message = """\
-Subject : Hi,
-
-my name is jacob."""
 
 port = 465
 sender_email = input("Enter sender email")
 sender_password = input('Enter your password')
 receiver_email = input("Enter receiver email")
 
+message = MIMEMultipart("alternative")
+message["Subject"] = "EmailAutomation project"
+message["From"] = sender_email
+message["To"] = receiver_email
+
+text = """\
+Hi,
+This is my emailautomation project."""
+
+html = """\
+<html>
+    <body style="background-color:red;">
+        <p>Hi,<br>
+        This is my <a href="http://www.myproject.com">emailautomation</a> project.
+        </p>
+    </body>
+</html>"""
+
+text_message = MIMEText(text, "text")
+html_message = MIMEText(html, "html")
+
+message.attach(text_message)
+message.attach(html_message)
+
+
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
     server.ehlo()
     server.login(sender_email, sender_password)
-    server.sendmail(sender_email, receiver_email, message)
+    server.sendmail(sender_email, receiver_email, message.as_string())
